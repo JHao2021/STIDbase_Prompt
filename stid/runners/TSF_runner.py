79d10f2c3ef6d9842017ceaf99f56fcd7162b474
JHao2021 <1094076339@qq.com>
2025-02-08 11:28:44 +0800

    fix preprocession

--- stid/runners/TSF_runner.py
+++ stid/runners/TSF_runner.py
@@ -2,7 +2,7 @@ from typing import Dict
 
 import torch
 from .base_tsf_runner import BaseTimeSeriesForecastingRunner
-
+import time
 class TimeSpaceForecastingRunner(BaseTimeSeriesForecastingRunner):
     """
     A Simple Runner for Time Series Forecasting: 
@@ -47,6 +47,38 @@ class TimeSpaceForecastingRunner(BaseTimeSeriesForecastingRunner):
         data = data[:, :, :, self.target_features]
         return data
 
+    def remove_duplicates_topo(self, topo):
+        cleaned_topo = []
+        for pair in topo:
+            # 提取每个张量的第一个值（因为所有值都相同）
+            first_value = pair[0][0].item()  # 转换为 Python 标量
+            second_value = pair[1][0].item()  # 转换为 Python 标量
+            cleaned_topo.append([first_value, second_value])
+        return torch.tensor(cleaned_topo)
+
+
+    def remove_duplicates_subgraphs(self, subgraphs):
+        cleaned = []
+        for sub in subgraphs:
+            # 对每个子图中的 tensor 进行拼接并去重
+            unique_values = torch.cat([tensor.unique() for tensor in sub])
+            # 将去重后的结果存储为一个张量
+            cleaned.append(unique_values)
+        return cleaned
+    
+    def remove_duplicates_subgraphs_foreachnode(self, subgraphs):
+        cleaned = []
+        for sub in subgraphs:  # 遍历每个子图
+            for tensor_list in sub:  # 遍历子图中的每个张量列表
+                for tensor in tensor_list:  # 遍历张量列表中的每个张量
+                    # 提取唯一值并转换为整数
+                    sub_cleaned = []
+                    unique_value = int(torch.unique(tensor).item())
+                    sub_cleaned.append(unique_value)
+                    # 将每个子图的 list 转换为 tensor
+                    cleaned.append(torch.tensor(sub_cleaned))
+        return cleaned
+    
     def forward(self, data: Dict, epoch: int = None, iter_num: int = None, train: bool = True, **kwargs) -> Dict:
         """
         Performs the forward pass for training, validation, and testing. 
@@ -68,25 +100,20 @@ class TimeSpaceForecastingRunner(BaseTimeSeriesForecastingRunner):
         """
 
         # Preprocess input data
-        future_data, history_data, topo, data_name = data['target'], data['inputs'], data['topo'], data['data_name']
-        topo = topo[0, ...]
+        future_data, history_data, topo, data_name, subgraphs = data['target'], data['inputs'], data['topo'], data['data_name'], data['subgraphs']
+        topo = self.remove_duplicates_topo(topo)
         
-        # prompt的topo是边，不是邻接矩阵
-        edges = []
-        for i in range(topo.size(0)):
-            for j in range(i + 1, topo.size(1)):  # 只考虑上三角部分以避免重复
-                if topo[i, j] != 0:
-                    edges.append((i, j))
-        edges = torch.tensor(edges)
+        # start_time = time.time()
+        subgraphs = self.remove_duplicates_subgraphs(subgraphs)
+        # end_time = time.time()
+        # execution_time = end_time - start_time
+        # print(f"执行时间: {execution_time:.6f} 秒")
 
         data_name = data_name[0]
         history_data = self.to_running_device(history_data)  # Shape: [B, L, N, C]
         future_data = self.to_running_device(future_data)    # Shape: [B, L, N, C]
-        edges = self.to_running_device(edges)
-        
+        topo = self.to_running_device(topo)
 
-        subgraphs = []
-        subgraphs.append(torch.arange(0, topo.size(0)))
 
         batch_size, length, num_nodes, _ = future_data.shape
 
@@ -105,7 +132,7 @@ class TimeSpaceForecastingRunner(BaseTimeSeriesForecastingRunner):
             batch_seen=iter_num,
             epoch=epoch,
             train=train,
-            topo=edges,  
+            topo=topo,  
             data_name=data_name,  
             mask_ratio=0.5,  
             mask_strategy='causal',  
